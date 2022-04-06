6027d047e72530a04c5ba2f6ad8f434fc1c21478
balidinesh18 <67181766+balidinesh08@users.noreply.github.com>
2022-04-06 07:22:38 +0530

    DB

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getUserById,
+  getUser,
+  updateUser
+} = require("../controllers/user");
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+
+router.param("userId", getUserById);
+
+router.get("/user/:userId/getuser", isSignedIn, isAuthenticated, getUser);
+router.put("/user/:userId/updateuser", isSignedIn, isAuthenticated, updateUser);
+
+// router.post("/user/:userId/blockedusers",isSignedIn,isAuthenticated,getBlockedUsers);
+
+
+module.exports = router;
